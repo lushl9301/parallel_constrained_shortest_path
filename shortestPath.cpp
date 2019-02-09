6d5e9df9deeb77d5a6fb69b993ba179dec6f4d52
lushl9301 <lushl9301@gmail.com>
2019-02-09 11:30:32 +0800

    <ref>

--- shortestPath.cpp
+++ shortestPath.cpp
@@ -1,5 +1,3 @@
-
-
 //*******************************************************************************************************
 //******************                                                                                *****
 //******************  A parallel algorithm for constrained shortest path problem                    *****
@@ -22,55 +20,47 @@
 // compile with C++ -I /usr/local/boost_1_59_0 -framework opencl shortestPath.cpp
 
 
-#include "percolation_graph_io.cpp"
+#include "percolation_graph_io.hpp"
+#include "percolation_parallel.hpp"
 
-#include "percolation_parallel.cpp"
+int main() {
 
+    srand((unsigned) time(0));
 
+    myint oldDim, dim, n, maxTime, maxCost, numTh, debuggingMode, cpuForce, threadForce;
+    myint avoidBoost, bVC, bChoice, tFP1, tFP2;
+    getSetupFromFile("setup.txt",
+                     &numTh,
+                     &oldDim,
+                     &dim,
+                     &maxTime,
+                     &maxCost,
+                     &n,
+                     &bVC,
+                     &bChoice,
+                     &debuggingMode,
+                     &cpuForce);
 
-int main(){
-    
-    srand((unsigned)time(0));
-    
-    
-    myint oldDim, dim,n, maxTime, maxCost, numTh, debuggingMode, cpuForce, threadForce, avoidBoost, bVC, bChoice, tFP1, tFP2;
-    getSetupFromFile("setup.txt", &numTh, &oldDim, &dim, &maxTime, &maxCost, &n , &bVC, &bChoice, &debuggingMode, &cpuForce);
-    
-    Graph **pgr,**pgr2;
-    pgr=new Graph*;
-    pgr2=new Graph*;
-    int totalTime=0;
+    Graph **pgr, **pgr2;
+    pgr = new Graph *;
+    pgr2 = new Graph *;
+    int totalTime = 0;
     int glj;
- 
-        readFromFile("example.txt",pgr);
-        copyGraph(*pgr,pgr2);
-    
-    shortestPathParallel(*pgr2);
-    
- 
+
+//    readFromFile("example.txt", pgr);
+//    copyGraph(*pgr, pgr2);
+//
+//    shortestPathParallel(*pgr2);
+
+//    delete *pgr;
+//    delete pgr;
+//    delete *pgr2;
+//    delete pgr2;
+
+    pgr = new Graph *;
+
+    createBigGraph(pgr, dim, n, maxTime, maxCost, bVC, bChoice);
+    printToFile("newRandomGraph.txt", *pgr, 1);
     delete *pgr;
     delete pgr;
-    delete *pgr2;
-    delete pgr2;
-    
- 
-    pgr=new Graph*;
-    
-    
-    
-    
-    createBigGraph(pgr, dim,n,maxTime, maxCost, bVC, bChoice);
-    printToFile("newRandomGraph.txt",*pgr,1);
-    delete *pgr;
-    delete pgr;
-    
-    
-    
-    
-    
-    
-    
-    
-    return 1;
-    
 }
